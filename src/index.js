fb9f10abaa92ddfd446de38c7713b9df8e92a27e
Vinícius Sales <vinicius.sbs@gmail.com>
2023-07-23 12:39:51 -0300

    feat: extrai links de matérias da página do time
    Extrai os links das demais matérias que aparecem abaixo das headlines
    da página do time.
    
    Deixei um TODO para a extração das headlines, porque elas são
    carregadas dimanicamente pela página e vão dar mais trabalho.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
+const logger = require('./logger')('index');
 const geTeamPageScraper = require('./scrapers/ge/team-page');
 
-geTeamPageScraper.main();
+async function run () {
+    const result = await geTeamPageScraper.scrape('Botafogo');  
+    logger.log(result);
+};
+
+run();
+
--- src/scrapers/ge/team-page.js
+++ src/scrapers/ge/team-page.js
@@ -2,41 +2,53 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const logger = require('../../logger')('ge/team-page');
 
-const recebeURL = async (url) => {
-    const result = await axios.get(url)
-    return result.data;
+exports.scrape = async (team) => {
+    logger.log(`Started scraping ${team} page...`);
+
+    const { $, response } = await fetchTeamPageData(team.toLowerCase());
+    const { headlines, otherArticles } = await parse($, response);
+
+    logger.log(`Finished scraping ${team} page.`);
+
+    return { headlines, otherArticles };
+};
+
+async function fetchTeamPageData (team) {
+    const response = await axios.get(`https://ge.globo.com/futebol/times/${team}/`);
+    const $ = cheerio.load(response.data);
+
+    return { $, response };
 }
 
-exports.main = async () => {
-    logger.log('Starting...');
+async function parse ($) {
+    function getURL () {
+        return $(this).prop('href');
+    }
 
-    const conteudoDaUrl = await recebeURL("https://ge.globo.com/futebol/times/botafogo/")
-    const $ = cheerio.load(conteudoDaUrl)
+    const $headlines = $('.bstn-hl-link').map(getURL);
+    const $otherArticles = $('.feed-post-link').map(getURL);
 
-    /*const exibeDados = $('h1').text()*/
-    /*const exibeDados = $('h2').text()*/
-    /*const exibeDados = $('title').text()
-    console.log(exibeDados);*/
+    // TODO: pegar headlines
+    // Quando a página é carregada, existe esse markup pra cada headline:
+    //
+    //      <div class="glb-skeleton-container"><div class="bstn-headline-skeleton glb-skeleton-box"></div></div>
+    //
+    // Depois de carregada, as headlines são carregadas por JS, que substitui esse markup pelo conteúdo real.
+    // Pra pegar esses dados, vamos precisar entender de onde esse dado vem.
+    //
+    // Como fiz pra reproduzir:
+    // 
+    // 1. fiz log da tag <main> e vi que foi retornada
+    // 2. fui adentrando 1 nível de cada vez até que nada mais fosse retornado
+    // 3. ao fazer log de <div clas="bstn-hl">, vi que estava sendo retornado um markup diferente do esperado
 
+    // DEBUG:
+    logger.debug($('.bstn-hl').html());
 
-    //Função que encontra links de materia e retora com os links ordenados
-    /*$('h2').each((i, e) => {
-        console.log(`O h2 na posição ${i} tem o valor igual a: ${$(e).text()}`);
-    })*/
-
-    let materias = []
-
-    //Função que acessa os links e extrai titulos e subtitulos
-    $('h2').each((i, e) => {
-        //informar o caminiha ate o chegar ao a e pega o valor do texto que esta nele
-        const tituloMateria= $(e).find('a > ul> li > div > div > h2').text();
-        const subtituloMateria = "https://ge.globo.com/futebol/times/botafogo/" + $(e).find('li > div > div > span').attr("src");
-
-        //Criando um obj com o valores a serem retornados 
-        const data = { tituloMateria, subtituloMateria}
-        materias.push(data);
-
-    })
-
-    logger.log('Finished.');
+    return {
+        headlines: Array.from($headlines),
+        otherArticles: Array.from($otherArticles)
+    };
 }
+
+
